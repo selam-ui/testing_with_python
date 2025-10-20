f47ce56ef6e9f220fa6b44b05169bdfbb2444e85
selam-ui <selamtsehsye27@gmail.com>
2025-10-20 23:10:00 +0300

    solved p3

--- Problem_3_Simple_Calculator/solution.py
+++ Problem_3_Simple_Calculator/solution.py
@@ -1,1 +1,22 @@
-# your code here
+# first we have to input 2 numbers
+num1 = float(input("enter first number:"))
+num2 = float(input("enter second number:"))
+# next we input operations
+op = input("enter opration (+,-,*,/):")
+#in the next step we calculate
+if op == '+':
+    result = num1 + num2
+elif op == '-':
+    result = num1 - num2
+elif op == '*':
+    result = num1 * num2
+elif op == '/':
+    if num2 == 0:
+        result ="error: division by zero!"
+    else:
+        result = num2 / num1
+else:
+    result = "invalid operation!"
+print("result:", result)
+
+
