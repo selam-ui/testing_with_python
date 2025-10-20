03a08f09711626d4eb7d49d1cadb14deba4793eb
selam-ui <selamtsehsye27@gmail.com>
2025-10-21 00:15:25 +0300

    solved p5

--- Problem_5_Password_Validator/solution.py
+++ Problem_5_Password_Validator/solution.py
@@ -1,1 +1,19 @@
-# your code here
+def password_validator():
+    name = input("enter your name:")
+    password = input("enter your password:")
+
+    missing_rule = []
+    if len(password) < 8:
+        missing_rule.append("password should be at least six characters long.")
+    if not any(char.isdigit() for char in password):
+        missing_rule.append("password should contain at least two digit.")
+    if password.lower() == name.lower():
+        missing_rule.append("password should not be the same as your name.")
+    if not missing_rule:
+        print("password strength: STRONG")
+    else:
+        print("password strength: WEAK")
+        print("missing rule:")
+        for rule in missing_rule:
+            print("-", rule)
+password_validator()
