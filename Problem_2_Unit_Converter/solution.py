a782e47da95a9dda4032d6f4783dccd4e1d2799f
selam-ui <selamtsehsye27@gmail.com>
2025-10-20 22:15:36 +0300

    solved p2

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,17 @@
-# your code here
+def unit_converter():
+    print("which converstion would you like?")
+    print("1, kilometer to miles")
+    print("2, miles to kilometer")
+    choice = input ("enter 1 or 2 :")
+
+    if choice == '1':
+       value = float (input("enter value kilometers:"))
+       result = value * 0.621371
+       print(f"{value} kilometers is {result:.2f} miles.")
+    elif choice == '2':
+       value = float (input("enter value miles:"))
+       result = value/0.621371
+       print(f"{value} miles is {result:.2f} kilometers.")
+    else:#if the user enter strings or numbers that doesn't exist in the choice
+        print("invalid choice.")
+unit_converter()
