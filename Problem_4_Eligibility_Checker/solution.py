e69c8e86b9b7117417dbd57865d4ee87c5898a5f
selam-ui <selamtsehsye27@gmail.com>
2025-10-20 23:31:30 +0300

    solved p4

--- Problem_4_Eligibility_Checker/solution.py
+++ Problem_4_Eligibility_Checker/solution.py
@@ -1,1 +1,11 @@
-# your code here
+# first we have to get users age and license status
+age = int(input("enter your age"))
+license_status = (input("do you have a driving license? (yes/no:)")). lower()
+#the next step is nested if/else to check eligibility
+if age>=18:
+    if license_status == "yes":
+        print("you are eligiable to drive.")
+    else: 
+        print("you are old enough but do not have a license.")
+else:
+    print("you are not enough old enough to drive.")
